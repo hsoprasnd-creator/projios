0fa5095ce4a0665047fca01ee352f5155f5a5a62
hsoprasnd-creator <hsoprasnd@gmail.com>
2026-09-03 15:36:32 +0530

    Delete sabji directory

--- sabji/MemoryUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-#include <mach/mach.h>
-
-uintptr_t getBaseAddress();
-template<typename T> T readMemory(uintptr_t address);
-template<typename T> void writeMemory(uintptr_t address, T value);
-uintptr_t readPtr(uintptr_t address);
--- sabji/OverlayView.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface OverlayView : UIView
-
-- (void)showMenu;
-- (void)hideMenu;
-- (BOOL)isMenuVisible;
-
-@end
